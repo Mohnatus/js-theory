abef58546bbe87da45d46e7c36c93d87e8c25eaa
korneeva-m <korneeva.m@bookcentre.ru>
2019-02-11 15:41:57 +0300

    clear

--- configs/webpack/main.js
+++
@@ -1,6 +0,0 @@
-import {one, two} from './nums';
-console.log(`Sum: ${one + two}`);
-let sum = one + two;
-document.addEventListener('click', function() {
-  console.log(++sum);
-});
--- configs/webpack/nums.js
+++
@@ -1,3 +0,0 @@
-export let one = 1;
-let two = 2;
-export { two };
